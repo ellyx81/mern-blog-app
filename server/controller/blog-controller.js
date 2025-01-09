17cfe2d4443d0b616ac9f310b492d275e36cd0ae
giselle <gisellesan0801@gmail.com>
2025-01-09 22:56:36 +0800

    Add main functionalities - Add blog - Edit blog - Delete blog

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Blog = require('../model/Blog');
+const mongoose = require("mongoose");
+const Blog = require("../model/Blog");
 
 //fetch list of blogs
 //add a new blog
@@ -7,84 +7,92 @@ const Blog = require('../model/Blog');
 //update a blog
 
 const fetchListOfBlogs = async (req, res) => {
-    let blogList;
-    try {
-        blogList = await Blog.find()
-    } catch (e) {
-        console.log(e);
-    }
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (e) {
+    console.log(e);
+  }
 
-    if (!blogList) {
-        return res.status(404).json({ message: 'No Blogs Found' })
-    }
+  if (!blogList) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
 
-    return res.status(200).json({ blogList });
-}
+  return res.status(200).json({ blogList });
+};
 
 const addNewBlog = async (req, res) => {
-    const { title, description } = req.body;
-    const currentDate = new Date();
+  const { title, description } = req.body;
+  const currentDate = new Date();
 
-    const newlyCreateBlog = new Blog({
-        title, description, date: currentDate
-    })
+  const newlyCreateBlog = new Blog({
+    title,
+    description,
+    date: currentDate,
+  });
 
-    try {
-        await newlyCreateBlog.save()
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await newlyCreateBlog.save();
+  } catch (e) {
+    console.log(e);
+  }
 
-    try {
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        await newlyCreateBlog.save(session);
-        session.commitTransaction();
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newlyCreateBlog.save(session);
+    session.commitTransaction();
+  } catch (e) {
+    return res.send(500).json({ message: e });
+  }
 
-    } catch (e) {
-        return res.send(500).json({ message: e });
-    }
-
-    return res.status(200).json({ newlyCreateBlog });
+  return res.status(200).json({ newlyCreateBlog });
 };
 
 const deleteABlog = async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const findCurrentBlog = await Blog.findByIdAndDelete(id);
-        if (!findCurrentBlog) {
-            return res.status(404).json({ message: 'Blog not found' })
-        }
-
-        return res.status(200).json({ message: 'Successfully Deleted.' });
-    } catch (e) {
-        console.log(e);
-        return res.status(500).json({ message: 'Unable to delete! Please try again.' })
+  try {
+    const findCurrentBlog = await Blog.findByIdAndDelete(id);
+    if (!findCurrentBlog) {
+      return res.status(404).json({ message: "Blog not found" });
     }
+
+    return res.status(200).json({ message: "Successfully Deleted." });
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(500)
+      .json({ message: "Unable to delete! Please try again." });
+  }
 };
 
 const updateABlog = async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const { title, description } = req.body
-    let currentBlogUpdate
+  const { title, description } = req.body;
+  let currentBlogUpdate;
 
-    try {
-        currentBlogUpdate = await Blog.findByIdAndUpdate(id, {
-            title, description
-        })
-    } catch (e) {
-        console.log(e);
+  try {
+    currentBlogUpdate = await Blog.findByIdAndUpdate(id, {
+      title,
+      description,
+    });
+  } catch (e) {
+    console.log(e);
 
-        return res.send(500).json({ message: 'Something went wrong while updating! Please try again.' });
-    }
+    return res
+      .status(500)
+      .json({
+        message: "Something went wrong while updating! Please try again.",
+      });
+  }
 
-    if (!currentBlogUpdate) {
-        return res.status(500).json({ message: 'Unable to update' });
-    }
+  if (!currentBlogUpdate) {
+    return res.status(500).json({ message: "Unable to update" });
+  }
 
-    return res.send(200).json({ currentBlogUpdate });
+  return res.status(200).json({ currentBlogUpdate });
 };
 
 module.exports = { fetchListOfBlogs, deleteABlog, updateABlog, addNewBlog };
