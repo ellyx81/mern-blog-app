afce0aacc0324ba09158a5dce3ad610b29716629
giselle <gisellesan0801@gmail.com>
2025-01-20 19:36:25 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const blogRouter = require('./route/blog-route');
+const express = require("express");
+const cors = require("cors");
+const blogRouter = require("./route/blog-route");
+const PORT = process.env.PORT || 5000;
 
 require("./db");
 
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use("/api/blogs", blogRouter);
 
 app.use("/api", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
-app.listen(5000, () => console.log(`App is running at 5000...`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
